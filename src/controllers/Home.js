a1e1f3f76ee1fa8e1c1e7631091634184d064a83
edwinhati <edwinhati@outlook.com>
2022-07-13 23:02:52 +0800

    change api source

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,20 +1,46 @@
 const request = require('request');
+
 exports.data = (req, res, next) => {
-    const url = 'https://covid19.mathdro.id/api/countries/indonesia';
+    const url = 'https://data.covid19.go.id/public/api/update.json';
 
     request({
         url: url,
         json: true
     }, function (error, response, body) {
-    
         if (!error && response.statusCode === 200) {
             res.json({
-                Country: "Indonesia",
-                UpdateDate: body.lastUpdate,
-                Confirmed: body.confirmed.value,
-                Recovered: body.recovered.value,
-                Deaths: body.deaths.value,
+                Date: body.update.penambahan.tanggal,
+                Confirmed: {
+                    Total : body.update.harian.at(-1).jumlah_positif_kum.value,
+                    New : body.update.penambahan.jumlah_positif
+                },Recovered: {
+                    Total : body.update.harian.at(-1).jumlah_sembuh_kum.value,
+                    New : body.update.penambahan.jumlah_sembuh
+                }, Deaths: {
+                    Total : body.update.harian.at(-1).jumlah_meninggal_kum.value,
+                    New : body.update.penambahan.jumlah_meninggal
+                }            
             })         
         }
     })
 }
+
+// exports.data = (req, res, next) => {
+//     const url = 'https://covid19.mathdro.id/api/countries/indonesia';
+
+//     request({
+//         url: url,
+//         json: true
+//     }, function (error, response, body) {
+    
+//         if (!error && response.statusCode === 200) {
+//             res.json({
+//                 Country: "Indonesia",
+//                 UpdateDate: body.lastUpdate,
+//                 Confirmed: body.confirmed.value,
+//                 Recovered: body.recovered.value,
+//                 Deaths: body.deaths.value,
+//             })         
+//         }
+//     })
+// }
