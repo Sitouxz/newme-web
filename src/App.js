026b0e9bde02cf484e07b39f35fb8763672ef39d
Sitou <yehezkielowen0901@gmail.com>
2022-07-16 06:34:47 +0800

    Home & navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+import Date from './components/Date';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4 bg-[#BCFFCE]">
+      <Header />
+      <Content />
+      <Date />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import contentImg from '../assets/img/content.png';
+
+function Content() {
+  return (
+    <div className="flex justify-center items-center my-20 sm:my-0">
+      <img src={contentImg} alt="Content" />
+    </div>
+  );
+}
+
+export default Content;
