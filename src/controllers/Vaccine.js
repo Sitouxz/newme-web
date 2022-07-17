17f17886cf1809d721868ed947e8a3e9e69d3ee5
edwinhati <edwinhati@outlook.com>
2022-07-17 23:01:48 +0800

    add initial vaccine api code

--- src/controllers/Vaccine.js
+++ src/controllers/Vaccine.js
@@ -1,8 +1,9 @@
 exports.main = (req, res, next) => {
-    res.json({
-        page: "Under Development",
-        more: 'https://newme-api.herokuapp.com/vaccine/information',
-        code: 'https://github.com/edwinhati'
+    
+    // res.json({
+    //     page: "Under Development",
+    //     more: 'https://newme-api.herokuapp.com/vaccine/information',
+    //     code: 'https://raw.githubusercontent.com/edwinhati/newme/api/src/controllers/Vaccine.js'
 
-    })
+    // })
 }
