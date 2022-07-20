d986cf222230605bf618abb2950848ae25cc187d
MG177 <mg.terok77@gmail.com>
2022-07-20 18:54:11 +0800

    corona berdansa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useRef, useEffect, Suspense } from 'react';
 import Header from '../components/Header';
 import Content from '../components/Content';
 import Date from '../components/Date';
 import { motion } from 'framer-motion';
 
 import { Vector3 } from 'three'
-import { useRef } from 'react'
-import {Html, SpotLight, useDepthBuffer, useGLTF} from '@react-three/drei';
+import {Html, SpotLight, useDepthBuffer, useGLTF, useAnimations} from '@react-three/drei';
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import {Section} from '../components/section';
 
@@ -35,21 +34,33 @@ function MovingSpot({ vec = new Vector3(), ...props }) {
   return <SpotLight castShadow ref={light} penumbra={1} distance={6} angle={0.35} attenuation={5} anglePower={4} intensity={2} {...props} />
 }
 
-
-function Scene() {
-  // This is a super cheap depth buffer that only renders once (frames: 1 is optional!), which works well for static scenes
-  // Spots can optionally use that for realism, learn about soft particles here: http://john-chapman-graphics.blogspot.com/2013/01/good-enough-volumetrics-for-spotlights.html
-  const depthBuffer = useDepthBuffer({ frames: 1 })
-  const { nodes, materials } = useGLTF('https://market-assets.fra1.cdn.digitaloceanspaces.com/market-assets/models/dragon/model.gltf')
+function Virus(props) {
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF('virus.glb');
+  const { actions } = useAnimations(animations, group);
+  useEffect(() => {
+    actions.VirusAction.play();
+  });
   return (
-    <>
-      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[3, 3, 2]} />
-      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[1, 3, 0]} />
-      <mesh position={[0, -1.03, 0]} castShadow receiveShadow geometry={nodes.dragon.geometry} material={materials['Default OBJ.001']} dispose={null} />
-      <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
-        <planeGeometry args={[50, 50]} />
+    <group ref={group} {...props} dispose={null}>
+      <mesh position={[0, -1.03, 0]} castShadow receiveShadow geometry={nodes.Virus.geometry} material={materials['Material.001']}/>
+      <mesh receiveShadow position={[0, -1, 0]} rotation-x={-Math.PI / 2} >
+        {/* <planeGeometry color="white" opacity={0.5} args={[90, 50]} transparant/> */}
         <meshPhongMaterial />
       </mesh>
+    </group>
+  )
+}
+
+function Scene() {
+  const depthBuffer = useDepthBuffer({ frames: 1 })
+  return (
+    <>
+    <Suspense fallback={null}>
+      <Virus />
+    </Suspense>
+      <MovingSpot depthBuffer={depthBuffer} color="#0c8cbf" position={[3, 3, 2]} />
+      <MovingSpot depthBuffer={depthBuffer} color="#b00c3f" position={[1, 3, 0]} />
     </>
   )
 }
@@ -66,6 +77,7 @@ export default function Home() {
        <Canvas camera={{ position: [-2, 2, 6], fov: 50, near: 1, far: 20 }}>
       <HTMLContent />
       <Scene />
+      
       </Canvas> 
       </div>
       <Date />
