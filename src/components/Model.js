3cbda56adc6d60dd3640932456fb8aa9eeaa728d
Sitou <yehezkielowen0901@gmail.com>
2022-07-22 13:01:47 +0800

    test gltf

--- src/components/Model.js
+++ src/components/Model.js
@@ -36,7 +36,7 @@ function Light({ vec = new Vector3(), ...props }) {
 
 function Virus(props) {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF('./model.glb');
+  const { nodes, materials, animations } = useGLTF('./virus.gltf');
   const { actions } = useAnimations(animations, group);
   useEffect(() => {
     actions.virusAction.play();
