725d4d41229edef8496cf296afc5e6c43e176cbb
edwinhati <edwinhati@outlook.com>
2022-07-26 18:36:31 +0800

    update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const Routes = require('./src/routes/Router');
-let port = process.env.PORT || 80;   
+let port = process.env.PORT || 3000;   
 app.use('/', Routes);
 
 // app.use((req, res, next) => {
