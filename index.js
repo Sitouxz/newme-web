29b0ac31ae678a7e14be1a9618cd675ac90c4e7b
edwinhati <edwinhati@outlook.com>
2022-07-27 19:23:20 +0800

    update CORS policy

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const express = require('express');
 const app = express();
 const Routes = require('./src/routes/Router');
 let port = process.env.PORT || 3000;   
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 app.use('/', Routes);
+
 app.listen(port, () => {});
